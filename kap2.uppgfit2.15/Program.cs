e0f78abe61daa2cacfa1de32846c0b22f5124fc3
Hugge25 <hugo.paulsson@hotmail.com>
2022-09-14 16:52:22 +0200

    Add project files.

---
+++ kap2.uppgfit2.15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kap2._uppgfit2._15
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv tre ord med punkt");
+            var ord = Console.ReadLine().Split(' ');
+            string a = ord[0];
+            string b = ord[1];
+            string c = ord[2];
+
+            c = c.TrimEnd('.');
+
+
+            Console.WriteLine(c + " " + b + " " + a + ".");
+        }
+    }
+}
